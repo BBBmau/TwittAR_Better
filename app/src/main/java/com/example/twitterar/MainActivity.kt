4fbd533a6739c12009d052feeb1b2443cedfb1ae
Mauricio Alvarez Leon <alvarez.mauricioTM@gmail.com>
2020-11-20 23:21:44 -0800

    Implemented the Camera Preview

--- app/src/main/java/com/example/twitterar/MainActivity.kt
+++ app/src/main/java/com/example/twitterar/MainActivity.kt
@@ -1,13 +1,106 @@
 package com.example.twitterar
 
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Application
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraXConfig
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
 
+class CameraX : Application(),  CameraXConfig.Provider {
+    override fun getCameraXConfig(): CameraXConfig {
+        return Camera2Config.defaultConfig()
+    }
+}
+// github = 14b4cc74d48e4e2da398cc965d4f37c0899c28c1
 class MainActivity : AppCompatActivity() {
+    private val CAMERA_PERMISSION_CODE = 1
+
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Request camera permissions
+        if(ContextCompat.checkSelfPermission(this,
+            Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            startCamera()
+        } else{
+            requestCameraPermission()
+
+        }
+
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+
+
+
+    private fun bindPreview(cameraProvider : ProcessCameraProvider) {
+        val viewFinder = findViewById<PreviewView>(R.id.viewFinder)
+        var preview : Preview = Preview.Builder()
+                .build()
+
+        var cameraSelector : CameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+        preview.setSurfaceProvider(viewFinder.surfaceProvider)
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
+    private fun startCamera(){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    private fun requestCameraPermission(){
+       if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+            AlertDialog.Builder(this)
+                .setTitle("Camera Needed")
+                .setMessage("Camera needed for Augmented Reality")
+
+       }else {
+           ActivityCompat.requestPermissions(this,
+               arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+       }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
 
 }
