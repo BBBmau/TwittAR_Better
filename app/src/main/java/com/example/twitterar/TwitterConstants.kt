2532542dea3598213e8ef55844cf886ad4f981a9
Denielle Abaquita <denielle.abaquita@gmail.com>
2020-11-20 20:45:23 -0500

    Twitter-Login : Implemented login button but it won't do anything despite the implementations

---
+++ app/src/main/java/com/example/twitterar/TwitterConstants.kt
@@ -0,0 +1,9 @@
+package com.example.twitterar
+
+object TwitterConstants {
+
+    var CONSUMER_KEY = "855211469620871168-jGoBctl9iBYUOT1z5a4jerWF4d3gwHz"
+    var CONSUMER_SECRET = "lzPp9IJ1YkUW00kCMElnNFEFIdTQ9l9hZhK5BPrIU1bt2"
+    var CALLBACK_URL = "twitt.ar://"
+
+}
